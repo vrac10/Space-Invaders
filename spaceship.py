2e61dada656a090b843a1c622cef6bec5ad50f76
Namitha Rayasam <namitha@Namithas-MacBook-Pro.local>
2023-10-12 10:37:00 +0530

    Called you dumb

--- spaceship.py
+++ spaceship.py
@@ -39,6 +39,7 @@ class spaceship():
         if condition == True:
             pygame.draw.rect(surface , (255,0,0), self.bullet)
 
+    # why the fuck is this project so bad omg get good kid
 
     # def draw(self, surface):
     #     pygame.draw.rect(surface , (255,0,0), self.rect)
